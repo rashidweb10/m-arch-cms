a46fbf03e4c50adf8e74f9f966935fe32852f821
Muhammed rashid <rashidk.developer@gmail.com>
2025-12-28 15:09:34 +0530

    .

--- resources/views/frontend/auth/profile.blade.php
+++ resources/views/frontend/auth/profile.blade.php
@@ -122,7 +122,6 @@
         <div class="mb-3">
             <div class="alert alert-info alert-dismissible fade show" role="alert">
                 <i class="fab fa-google me-2"></i> This account is linked with Google.
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         </div>
         @endif
