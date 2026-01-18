1e0c853e5f724c1f2f8931aa8bb08a8333696692
Muhammed rashid <rashidk.developer@gmail.com>
2026-01-18 16:30:20 +0530

    .

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -22,8 +22,6 @@
   $achievement_title = $pageData->meta->where('meta_key', 'achievement_title')->first()->meta_value ?? '';
   $achievement_description = $pageData->meta->where('meta_key', 'achievement_description')->first()->meta_value ?? '';
   $achievement_image = $pageData->meta->where('meta_key', 'achievement_image')->first()->meta_value ?? ''; 
-  
-  $home_awards = json_decode($pageData->meta->where('meta_key', 'home_awards')->first()->meta_value ?? '[]', true);
 
   $video = $pageData->meta->where('meta_key', 'video')->first()->meta_value ?? '';
 
@@ -305,38 +303,6 @@
       </div>
    </div>
 </section>
-
-@if(isset($home_awards['itration']) && is_array($home_awards['itration']))
-<!--<section class="awards_achievements pt-4 pt-md-5 pb-0 position-relative" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">-->
-<!--   <div class="container">-->
-<!--      <div class="row justify-content-center">-->
-<!--         <div class="col-lg-12 aos-init aos-animate">-->
-<!--            <div class="text-start mb-md-4 mb-2 pt-2">-->
-<!--               <h3 class="roboto text_color text-center robot_slab">Awards & Accolades</h3>-->
-<!--            </div>-->
-<!--         </div>-->
-<!--         <div class="row">-->
-
-<!--            @foreach($home_awards['itration'] as $index => $itration)-->
-<!--            <div class="col-md-3">-->
-<!--               <div class="aos-init aos-animate position-relative">-->
-<!--                  <div class="about_border border_9 position-relative">-->
-<!--                     <a href="{{ uploaded_asset($home_awards['image'][$index]) }}" data-fancybox="gallery1">-->
-<!--                     <img class="jbox-img rotate w-100 hvr-bounce-in" src="{{ uploaded_asset($home_awards['image'][$index]) }}" alt="{{ central_asset(uploaded_asset($home_awards['title'][$index])) }}">-->
-<!--                     </a>-->
-<!--                  </div>-->
-<!--               </div>-->
-<!--            </div>-->
-<!--            @endforeach-->
-
-<!--         </div>-->
-<!--         {{-- <div class="read-more text-center mt-4">-->
-<!--            <a href="/" class="btn-2 robot_slab">View All</a>-->
-<!--         </div> --}}-->
-<!--      </div>-->
-<!--   </div>-->
-<!--</section>-->
-@endif
 
 @if(!empty($video))
 <section class="awards_achievements pt-4 pt-md-5 pb-0 position-relative" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
