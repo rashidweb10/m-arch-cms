77fbd04f7306943c28e5d91b3f47e8fefd7e1919
Mahtab645 <ansarimehtab645@gmail.com>
2026-01-21 20:20:59 +0530

    Update WhatsApp button position and z-index in CSS; modify footer to include a link to Maptek

--- resources/views/backend/partials/footer.blade.php
+++ resources/views/backend/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="page-container">
         <div class="row">
             <div class="col-md-12 text-center">
-                <script>document.write(new Date().getFullYear())</script> © {{ config('app.name') }} - By <span class="fw-bold text-decoration-underline text-uppercase text-reset fs-12">{{ config('custom.author') }}</span>
+               <script>document.write(new Date().getFullYear())</script> © {{ config('app.name') }} - By <span class="fw-bold text-decoration-underline text-uppercase text-reset fs-12">  <a target="_blank" href="https://www.maptek.in/">Maptek</a></span>
             </div>
         </div>
     </div>
