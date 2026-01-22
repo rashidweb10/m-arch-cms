06fc303ed3b501c918a6c77567fd919c17996392
Muhammed rashid <rashidk.developer@gmail.com>
2026-01-22 21:37:35 +0530

    3

--- resources/views/backend/students/index.blade.php
+++ resources/views/backend/students/index.blade.php
@@ -100,11 +100,15 @@
                                     {{ $row->is_active ? 'Active' : 'Inactive' }}
                                 </span>                                    
                                 </td>
-                                <td>{{ $row->course_enrolments_count ?? 0 }}</td>
+                                <td>
+                                    <a target="_blank" href="{{ route('course-enrolments.index', ['search' => $row->email]) }}" class="">
+                                        {{ $row->course_enrolments_count ?? 0 }}
+                                    </a>
+                                </td>
                                 <td>{{ formatDatetime($row->created_at) }}</td>
                                 <td>{{ formatDatetime($row->updated_at) }}</td>                                
                                 <td>
-                                    <a target="_blank" href="{{ route('course-enrolments.index', ['search' => $row->email]) }}" class="link-reset fs-20 p-1"><i class="ti ti-books"></i></a>
+                                    {{-- <a target="_blank" href="{{ route('course-enrolments.index', ['search' => $row->email]) }}" class="link-reset fs-20 p-1"><i class="ti ti-books"></i></a> --}}
                                     <a href="javascript:void(0);" onclick="smallModal('{{url(route('students.edit', $row->id))}}', 'Edit')" class="link-reset fs-20 p-1"> <i class="ti ti-pencil"></i></a>
                                     <a href="javascript:void(0);" onclick="confirmModal('{{ route('students.destroy', $row->id) }}', callbackStudents )" class="link-reset fs-20 p-1"> <i class="ti ti-trash"></i></a>
                                     <a href="javascript:void(0);" onclick="loginAsStudent('{{ $row->id }}')" class="link-reset fs-20 p-1"> <i class="ti ti-user-check"></i></a>
