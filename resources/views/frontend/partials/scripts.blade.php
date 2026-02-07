f677a5e2ab211d07993b690cda09837a04182f30
Muhammed rashid <rashidk.developer@gmail.com>
2026-02-07 22:29:09 +0530

    content protection added

--- resources/views/frontend/partials/scripts.blade.php
+++ resources/views/frontend/partials/scripts.blade.php
@@ -1,3 +1,4 @@
+<script src="{{ asset('assets/frontend/js/content-protect.js') }}" defer></script>
 <script src="{{ asset('assets/frontend/js/jquery-3.6.0.min.js') }}"></script>
 <script src="{{ asset('assets/frontend/js/popper.min.js') }}"></script>
 <script src="{{ asset('assets/frontend/js/bootstrap.min.js') }}"></script>
